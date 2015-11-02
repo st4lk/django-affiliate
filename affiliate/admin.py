8a3ba4c71526e7caa5effdabf8e04b2c8517cc46
st4lk <myhappydo@gmail.com>
2015-11-02 16:06:15 +0300

    admin: inlude user in raw_id_fields

--- affiliate/admin.py
+++ affiliate/admin.py
@@ -9,6 +9,7 @@ Affiliate = get_model(app_settings.AFFILIATE_MODEL)
 class AffiliateAdmin(admin.ModelAdmin):
     list_display = ('pk', 'user', 'is_active', 'reward_amount',
         'reward_percentage', 'created_at')
+    raw_id_fields = ('user', )
 
 
 admin.site.register(Affiliate, AffiliateAdmin)
